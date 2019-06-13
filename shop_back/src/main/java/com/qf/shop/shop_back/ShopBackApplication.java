db325afafedef5e2c0c88ea1efa6dbe91c33e575
ken_global <ken_global@qq.com>
2019-06-13 20:32:34 +0800

    集成swagger

--- shop_back/src/main/java/com/qf/shop/shop_back/ShopBackApplication.java
+++ shop_back/src/main/java/com/qf/shop/shop_back/ShopBackApplication.java
@@ -1,7 +1,12 @@
 package com.qf.shop.shop_back;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 
@@ -14,24 +19,24 @@ public class ShopBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopBackApplication.class, args);
     }
-//    @Bean
-//    public Queue getQueue1(){
-//        return new Queue("solr_index_queue");
-//    }
-//    @Bean
-//    public Queue getQueue2(){
-//        return new Queue("create_html_queue");
-//    }
-//    @Bean
-//    public FanoutExchange getFanoutExchange(){
-//        return new FanoutExchange("goods_exchange");
-//    }
-//    @Bean
-//    public Binding getBinding1(Queue getQueue1, FanoutExchange getFanoutExchange){
-//        return BindingBuilder.bind(getQueue1).to(getFanoutExchange);
-//    }
-//    @Bean
-//    public Binding getBinding2(Queue getQueue2, FanoutExchange getFanoutExchange){
-//        return BindingBuilder.bind(getQueue2).to(getFanoutExchange);
-//    }
+    @Bean
+    public Queue getQueue1(){
+        return new Queue("solr_index_queue");
+    }
+    @Bean
+    public Queue getQueue2(){
+        return new Queue("create_html_queue");
+    }
+    @Bean
+    public FanoutExchange getFanoutExchange(){
+        return new FanoutExchange("goods_exchange");
+    }
+    @Bean
+    public Binding getBinding1(Queue getQueue1, FanoutExchange getFanoutExchange){
+        return BindingBuilder.bind(getQueue1).to(getFanoutExchange);
+    }
+    @Bean
+    public Binding getBinding2(Queue getQueue2, FanoutExchange getFanoutExchange){
+        return BindingBuilder.bind(getQueue2).to(getFanoutExchange);
+    }
 }
